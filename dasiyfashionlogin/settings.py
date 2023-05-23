be7c9d84ac054078c7f7b232aeb038a4fdb2a064
Abrar Hussain <abrardar988651@gmail.com>
2023-05-23 12:21:06 +0530

    mysql

--- dasiyfashionlogin/settings.py
+++ dasiyfashionlogin/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'y+5y)04lrfc&08fr1k%^58yxf9=75c=!l@5cbevr5=f2l4k-r$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
